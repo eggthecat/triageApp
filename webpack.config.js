bdd824bf981e9dab30f613dc88b575767cfc234e
Student <example@example.com>
2019-09-13 13:08:32 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+new CleanWebpackPlugin(['dist']),
+new HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   mode: 'development',
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'triageApp',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
